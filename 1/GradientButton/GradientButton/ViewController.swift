3384199bff01636b742b9947f1af26531e943dba
Anna Mikhaylova <amikhaylova@axmor.com>
2024-02-02 20:35:55 +0500

    Ready

---
+++ 1/GradientButton/GradientButton/ViewController.swift
@@ -0,0 +1,55 @@
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let gradientView: UIView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setLayout()
+        setDesign()
+    }
+
+    private func setLayout() {
+        view.addSubview(gradientView)
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 100.0).isActive = true
+        gradientView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+    }
+
+
+    private func setDesign() {
+        gradientView.layer.insertSublayer(makeGradientLayer(), at: 0)
+        gradientView.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        gradientView.layer.shadowRadius = 16.0
+        gradientView.layer.shadowColor = UIColor.black.cgColor
+        gradientView.layer.shadowOpacity = 0.5
+    }
+
+    private func makeGradientLayer() -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.colors = [
+            UIColor(hex: 0xF05454).cgColor,
+            UIColor(hex: 0x5454F0).cgColor
+        ]
+        gradientLayer.cornerRadius = 16.0
+        return gradientLayer
+    }
+}
+
+extension UIColor {
+    convenience init(hex: Int) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
